f84dde7d33dede27158ffeeb40e39050ee52a5ec
epicyclism <paul@epicyclism.com>
2021-12-13 12:43:41 +0000

    Remove unnecessary print

--- aoc13/aoc13.cpp
+++ aoc13/aoc13.cpp
@@ -100,7 +100,6 @@ std::pair<paper, std::vector<fold>> get_input()
     }
     auto mx = std::max_element(tmp.begin(), tmp.end(), [](auto a, auto b){return a.first < b.first ;})->first;
     auto my = std::max_element(tmp.begin(), tmp.end(), [](auto a, auto b){return a.second < b.second ;})->second;
-    std::cout << "max x = " << mx << ", max y = " << my << "\n";
     paper p;
     p.x_ = mx + 1;
     p.sx_ = p.x_;
