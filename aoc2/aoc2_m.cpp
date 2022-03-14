da29cf6523ebbba7882f5de5f2ced8b8ce48df6f
epicyclism <paul@epicyclism.com>
2022-03-14 18:22:48 +0000

    aoc2 alternative

--- aoc2/aoc2_m.cpp
+++ aoc2/aoc2_m.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <utility>
+#include <tuple>
 #include <numeric>
 
-using pt = std::pair<int, int>;
+using hd = std::pair<int, int>;
 
 auto get_input()
 {
-	std::vector<pt> rv;
+	std::vector<hd> rv;
 	while (std::cin)
 	{
 		std::string d;
@@ -34,14 +35,23 @@ auto get_input()
 	return rv;
 }
 
-int pt1(auto& in)
+int pt1(auto const& in)
 {
-	auto p1 = std::accumulate(in.begin(), in.end(), pt{ 0, 0 });
+	auto p1 = std::accumulate(in.begin(), in.end(), hd{ 0, 0 }, [](auto const& sum, auto const& val) { return hd{ sum.first + val.first, sum.second + val.second}; });
 	return p1.first * p1.second;
+}
+
+using ahd = std::tuple<int, int, int>;
+
+int pt2(auto const& in)
+{
+	auto p2 = std::accumulate(in.begin(), in.end(), ahd{ 0, 0, 0 }, [](auto const& sum, auto const& val) { return ahd{ std::get<0>(sum) + val.second, std::get<1>(sum) + val.first, std::get<2>(sum) + std::get<0>(sum) * val.first}; });
+	return std::get<1>(p2) * std::get<2>(p2);
 }
 
 int main()
 {
 	auto in{ get_input() };
-
+	std::cout << "pt1 = " << pt1(in) << "\n";
+	std::cout << "pt2 = " << pt2(in) << "\n";
 }
