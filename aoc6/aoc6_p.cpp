0725674af83d1bff48f0b62be2909fd78ec02caa
Paul <paul@epicyclism.com>
2022-07-16 14:42:55 +0100

    Make aoc15 correct

--- aoc6/aoc6_p.cpp
+++ aoc6/aoc6_p.cpp
@@ -5,7 +5,7 @@
 #include <numeric>
 #include <cctype>
 
-template<int X, int Y, typename T = int64_t> struct matrix2	: public std::array<T, X * Y>
+template<int X, int Y, typename T = int64_t> struct matrix2d : public std::array<T, X * Y>
 {
 	constexpr T get(int x, int y) const
 	{
@@ -15,7 +15,7 @@ template<int X, int Y, typename T = int64_t> struct matrix2	: public std::array<
 	{
 		return this->operator[](y* X + x);
 	}
-	template<int Z> constexpr friend  auto operator*(matrix2<X, Y, T> const& a, matrix2<Z, Y, T>const& b) -> std::remove_cvref_t<decltype(b)>
+	template<int Z> constexpr friend auto operator*(matrix2d<X, Y, T> const& a, matrix2d<Z, Y, T>const& b)
 	{
 		static_assert(X == Z || Z == 1, "matrices don't match for multiplication.");
 		std::remove_cvref_t<decltype(b)> rv{ 0 };
@@ -31,7 +31,16 @@ template<int X, int Y, typename T = int64_t> struct matrix2	: public std::array<
 	}
 };
 
-constexpr matrix2<9, 9, int64_t> act
+template<typename T> constexpr T power(T const& t, const int n)
+{
+	if (n == 1)
+		return t;
+	if (n & 1)
+		return power(t * t, n / 2) * t;
+	return power(t * t, n / 2);
+}
+
+constexpr matrix2d<9, 9, int64_t> fish_gen_txf
 {
 	0, 1, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 1, 0, 0, 0, 0, 0, 0,
@@ -44,33 +53,26 @@ constexpr matrix2<9, 9, int64_t> act
 	1, 0, 0, 0, 0, 0, 0, 0, 0,
 };
 
-template<typename T> constexpr T power(T const& t, int n)
-{
-	if (n == 1)
-		return t;
-	if (n & 1)
-		return power(t * t, n / 2) * t;
-	return power(t * t, n / 2);
-}
-
 auto get_input()
 {
-	matrix2<1, 9> ra{};
+	matrix2d<1, 9> ra{};
 	std::string ln;
 	std::getline(std::cin, ln);
 	std::for_each(ln.begin(), ln.end(), [&ra](auto c) { if (std::isdigit(c)) ++ra[c - '0']; });
 	return ra;
 }
 
-template<int N> int64_t simulate_fish(matrix2<1, 9> v)
+template<int N> auto simulate_fish(matrix2d<1, 9> v)
 {
-	v = power(act, N) * v;
+	v = power(fish_gen_txf, N) * v;
 	return std::accumulate(v.begin(), v.end(), 0ll);
 }
 
 int main()
 {
-	auto in = get_input();
-	std::cout << "pt1 " << simulate_fish<80>(in) << "\n";
-	std::cout << "pt2 " << simulate_fish<256>(in) << "\n";
+	auto cnts = get_input();
+	std::cout << "example after  80 days  " << simulate_fish<80>(matrix2d<1,9>{ 0LL, 1LL, 1LL, 2LL, 1LL, 0LL, 0LL, 0LL, 0LL }) << "\n";
+	std::cout << "example after 256 days  " << simulate_fish<256>(matrix2d<1,9>{ 0LL, 1LL, 1LL, 2LL, 1LL, 0LL, 0LL, 0LL, 0LL }) << "\n";
+	std::cout << "pt1 " << simulate_fish<80>(cnts) << "\n";
+	std::cout << "pt2 " << simulate_fish<256>(cnts) << "\n";
 }
